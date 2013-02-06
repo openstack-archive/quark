295998f1870157241043c70877a3f42e8dd472fa
Matt Dietz <matt.dietz@rackspace.com>
2013-02-06 23:34:10 +0000

    Forgot my NoAuth middleware hack and the generic TODO I've been keeping

---
+++ quark/api/auth.py
@@ -0,0 +1,48 @@
+# Copyright 2013 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import webob.dec
+import webob.exc
+
+from quantum import context
+from quantum import wsgi
+from quantum.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class NoAuthMiddleware(wsgi.Middleware):
+    """Return a fake token if one isn't specified."""
+
+    @webob.dec.wsgify(RequestClass=wsgi.Request)
+    def __call__(self, req):
+        if 'X-Auth-Token' not in req.headers:
+            user_id = req.headers.get('X-Auth-User', 'admin')
+            project_id = req.headers.get('X-Auth-Project-Id', 'admin')
+            res = webob.Response()
+            res.headers['X-Auth-Token'] = '%s:%s' % (user_id, project_id)
+            res.content_type = 'text/plain'
+            res.status = '204'
+            return res
+
+        token = req.headers['X-Auth-Token']
+        user_id, _sep, project_id = token.partition(':')
+        project_id = project_id or user_id
+        ctx = context.Context(user_id,
+                              project_id,
+                              is_admin=True)
+
+        req.environ['quantum.context'] = ctx
+        return self.application
