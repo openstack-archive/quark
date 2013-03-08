01b1646d62a9c04008d6a6b96ecec278066395c8
Amir Sadoughi <amir.sadoughi@gmail.com>
2013-03-08 00:03:17 -0600

    Added test for failure to allocate ip on fully allocated subnet.

--- quark/tests/test_ipam.py
+++ quark/tests/test_ipam.py
@@ -413,7 +413,59 @@ class TestQuarkIpamBase(test_base.TestBase):
                                           reuse_after)
 
     def test_allocate_ip_address_fully_allocated_subnet(self):
-        pass
+        tenant_id = 'foobar'
+        port_id = '123'
+        port_backend_key = '123'
+        port_device_id = '123'
+        reuse_after = 0
+
+        net = models.Network()
+        net['name'] = 'mynet'
+        net['tenant_id'] = tenant_id
+        self.context.session.add(net)
+        self.context.session.flush()
+
+        net = self.context.session.query(models.Network).first()
+
+        subnet = models.Subnet()
+        subnet['tenant_id'] = tenant_id
+        subnet['network_id'] = net['id']
+        subnet['cidr'] = '192.168.10.1/32'
+        self.context.session.add(subnet)
+        self.context.session.flush()
+
+        subnet = self.context.session.query(models.Subnet).first()
+
+        port = models.Port()
+        port['tenant_id'] = tenant_id
+        port['id'] = port_id
+        port['network_id'] = net['id']
+        port['backend_key'] = port_backend_key
+        port['mac_address'] = None
+        port['device_id'] = port_device_id
+        self.context.session.add(port)
+        self.context.session.flush()
+
+        port = self.context.session.query(models.Port).first()
+
+        ipaddress = models.IPAddress()
+        ipaddress['tenant_id'] = tenant_id
+        ipaddress['address_readable'] = '::ffff:192.168.10.0'
+        ipaddress['address'] = int(netaddr.IPAddress('::ffff:192.168.10.0'))
+        ipaddress['subnet_id'] = subnet['id']
+        ipaddress['network_id'] = net['id']
+        ipaddress['version'] = 4
+        ipaddress['deallocated'] = False
+        ipaddress['deallocated_at'] = None
+        port['ip_addresses'].extend([ipaddress])
+        self.context.session.add(ipaddress)
+        self.context.session.flush()
+
+        with self.assertRaises(exceptions.IpAddressGenerationFailure):
+            ipaddress = self.ipam.allocate_ip_address(self.context.session,
+                                                      net['id'],
+                                                      port['id'],
+                                                      reuse_after)
 
     def test_allocate_ip_address_multiple_subnets(self):
         pass
