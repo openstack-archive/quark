e629136806cd7b4e97ec883031ebf960a50b2b23
Amir Sadoughi <amir.sadoughi@gmail.com>
2013-03-11 12:34:46 -0500

    Wrote out test plan for ip addresses

--- quark/tests/test_quark_plugin.py
+++ quark/tests/test_quark_plugin.py
@@ -66,6 +66,30 @@ class TestSubnets(TestQuarkPlugin):
 
 
 class TestIpAddresses(TestQuarkPlugin):
+    # POST /ip_address
+    #    version (optional)
+    #    ip_address optional
+    #    network_id and device_id (or port_id)
+
+    # 1. Create IP Address with network and device id (success) 110
+    # 1b. create IP Address with invalid network and/or invalid device_id (failure)
+    # 2. Create IP Address with port id (success) 001
+    # 2b. Create IP Address with invalid port id (failure)
+    # 3. Create IP Address with all ids missing (failure) 000
+    # 4. Create IP Address with network id and not device id (failure) 100
+    # 5. Create IP Address with not network id and device id (failure) 010
+    # 6. Create IP Address with version specified (version == 6), success with ipv6 address
+    # 7. Create IP Address with version specified (version == 4), success with ipv4 address
+    # 8. Create IP Address with version specified (version == 10), failure
+    # 9. Create IP Address with specifc ip_address, ip doesn't exist already, associates success
+    # 10. Create IP Address with specific ip_address, ip does exist already, associates success
+    # 11. Create IP Address with specific ip_address, fail when subnet doesn't exist
+
+    # POST /ip_address/id/ports: <== pass in a list of port_id’s
+
+    # 1. Fail: IP Address id doesn't exist
+    # 2. Fail: port_id in port_ids doesn't exist
+    # 3. Success: Associate ipaddress at specific id with port
     def test_create_ip_address_success(self):
         pass
 
