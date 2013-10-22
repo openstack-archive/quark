70291c0776e176c7d10dabc41d75eafd08128adb
Matt Dietz <matt.dietz@rackspace.com>
2013-10-22 20:14:22 +0000

    Fixes RM3256
    Fixes an accidental regression in network deletion caused when trying to
    fix IPAM. More specifically, the allocated_ips relationship was being
    improperly used earlier and was thus modified to only count IP addresses
    that had actually been allocated previously but weren't currently
    allocated. Unfortunately, we missed the case in network delete where the
    plugin code checks to see if any previously generated IP addresses are
    currently allocated. The tests passed because we stub out any database
    access, side-stepping the check.
    
    The fix reverts the allocated_ips relationship and adds a new
    "generated_ips" relationship for IPAM to utilize instead. Additionally,
    adds three tests and a new "functional" subfolder, as the only way to
    test these relationships is to actually hit the database.

---
+++ quark/tests/functional/plugin_modules/test_networks.py
@@ -0,0 +1,77 @@
+# Copyright 2013 Openstack Foundation
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for# the specific language governing permissions and limitations
+#  under the License.
+
+import contextlib
+from neutron.common import exceptions
+from neutron import context
+from neutron.db import api as neutron_db_api
+from neutron.openstack.common.db.sqlalchemy import session as neutron_session
+from oslo.config import cfg
+import unittest2
+
+from quark.db import api as db_api
+from quark.db import models
+import quark.ipam
+import quark.plugin
+
+
+class QuarkNetworkFunctionalTest(unittest2.TestCase):
+    def setUp(self):
+        self.context = context.Context('fake', 'fake', is_admin=False)
+        super(QuarkNetworkFunctionalTest, self).setUp()
+
+        cfg.CONF.set_override('connection', 'sqlite://', 'database')
+        neutron_db_api.configure_db()
+        models.BASEV2.metadata.create_all(neutron_session._ENGINE)
+
+    def tearDown(self):
+        neutron_db_api.clear_db()
+
+
+class QuarkDeleteNetworKDeallocatedIPs(QuarkNetworkFunctionalTest):
+    @contextlib.contextmanager
+    def _stubs(self, network, subnet, dealloc=True):
+        self.plugin = quark.plugin.Plugin()
+        self.ipam = quark.ipam.QuarkIpamANY()
+        with self.context.session.begin():
+            net_mod = db_api.network_create(self.context, **network)
+            subnet["network"] = net_mod
+            db_api.subnet_create(self.context, **subnet)
+            ip_addr = self.ipam.allocate_ip_address(self.context,
+                                                    net_mod["id"],
+                                                    0, 0)
+            if dealloc:
+                self.ipam._deallocate_ip_address(self.context, ip_addr[0])
+        yield net_mod
+
+    def test_delete_network_with_allocated_ips_fails(self):
+        network = dict(name="public", tenant_id="fake", network_plugin="BASE")
+        subnet = dict(id=1, ip_version=4, next_auto_assign_ip=2,
+                      cidr="0.0.0.0/24", first_ip=0, last_ip=255,
+                      ip_policy=None, tenant_id="fake")
+        with self._stubs(network, subnet, dealloc=False) as net_mod:
+            with self.assertRaises(exceptions.SubnetInUse):
+                self.plugin.delete_network(self.context, net_mod["id"])
+
+    def test_delete_network_with_deallocated_ips(self):
+        network = dict(name="public", tenant_id="fake", network_plugin="BASE")
+        subnet = dict(id=1, ip_version=4, next_auto_assign_ip=2,
+                      cidr="0.0.0.0/24", first_ip=0, last_ip=255,
+                      ip_policy=None, tenant_id="fake")
+        with self._stubs(network, subnet) as net_mod:
+            try:
+                self.plugin.delete_network(self.context, net_mod["id"])
+            except Exception:
+                self.fail("delete network raised")
---
+++ quark/tests/functional/test_ipam.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import contextlib
+
+from neutron import context
+from neutron.db import api as neutron_db_api
+from neutron.openstack.common.db.sqlalchemy import session as neutron_session
+from oslo.config import cfg
+import unittest2
+
+from quark.db import api as db_api
+from quark.db import models
+import quark.ipam
+
+
+class QuarkIpamBaseFunctionalTest(unittest2.TestCase):
+    def setUp(self):
+        self.context = context.Context('fake', 'fake', is_admin=False)
+        super(QuarkIpamBaseFunctionalTest, self).setUp()
+
+        cfg.CONF.set_override('connection', 'sqlite://', 'database')
+        neutron_db_api.configure_db()
+        models.BASEV2.metadata.create_all(neutron_session._ENGINE)
+
+    def tearDown(self):
+        neutron_db_api.clear_db()
+
+
+class QuarkIPAddressAllocate(QuarkIpamBaseFunctionalTest):
+    @contextlib.contextmanager
+    def _stubs(self, network, subnet):
+        self.ipam = quark.ipam.QuarkIpamANY()
+        with self.context.session.begin():
+            net_mod = db_api.network_create(self.context, **network)
+            subnet["network"] = net_mod
+            sub_mod = db_api.subnet_create(self.context, **subnet)
+        yield net_mod
+        with self.context.session.begin():
+            db_api.subnet_delete(self.context, sub_mod)
+            db_api.network_delete(self.context, net_mod)
+
+    def test_allocate_finds_no_deallocated_creates_new_ip(self):
+        network = dict(name="public", tenant_id="fake")
+        subnet = dict(id=1, ip_version=4, next_auto_assign_ip=2,
+                      cidr="0.0.0.0/24", first_ip=0, last_ip=255,
+                      ip_policy=None, tenant_id="fake")
+        with self._stubs(network, subnet) as net:
+            ipaddress = self.ipam.allocate_ip_address(self.context, net["id"],
+                                                      0, 0)
+            self.assertIsNotNone(ipaddress[0]['id'])
+            self.assertEqual(ipaddress[0]['address'], 2)
