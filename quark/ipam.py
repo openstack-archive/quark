e76d207a28ff0208642ca07336b261cbeb715c86
Jason Kölker <jason@koelker.net>
2014-04-20 06:48:19 +0000

    Delete IPs that are in policies
    Otherwise we spin finding the same ip over and over. This is a
    **HOTFIX** long term solution is to push the policy enforcement into the
    db query for addresses **OR** keep track of policy excluded ips, and
    filter them out in the next query.

--- quark/ipam.py
+++ quark/ipam.py
@@ -221,9 +221,9 @@ class QuarkIpam(object):
                                 addr = addr.ipv4()
                             else:
                                 addr = addr.ipv6()
-                            # NOTE(jkoelker) Should we delete the ip record?
-                            # TODO(mdietz): yes
+
                             if policy is not None and addr in policy:
+                                context.session.delete(address)
                                 continue
 
                             if addr in cidr:
