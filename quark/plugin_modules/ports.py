e006c5e59a6e1327b0b776c11cd314e3519f2ae6
Amir Sadoughi <amir.sadoughi@gmail.com>
2014-09-27 13:41:17 -0500

    Fix RM9305
    * Fix does not allow a tenant to associate a network to a port
    that does not belong to them unless it is publicnet or servicenet
    
    * In existing UT, adds tenant_id to network models
    
    * Added UT

--- quark/plugin_modules/ports.py
+++ quark/plugin_modules/ports.py
@@ -34,6 +34,15 @@ LOG = logging.getLogger(__name__)
 STRATEGY = network_strategy.STRATEGY
 
 
+# HACK(amir): RM9305: do not allow a tenant to associate a network to a port
+# that does not belong to them unless it is publicnet or servicenet
+def _raise_if_unauthorized(tenant_id, net):
+    rax_networks = ('00000000-0000-0000-0000-000000000000',
+                    '11111111-1111-1111-1111-111111111111')
+    if net["id"] not in rax_networks and net["tenant_id"] != tenant_id:
+        raise exceptions.NotAuthorized()
+
+
 def create_port(context, port):
     """Create a port
 
@@ -62,8 +71,10 @@ def create_port(context, port):
     port_id = uuidutils.generate_uuid()
 
     net = db_api.network_find(context, id=net_id, scope=db_api.ONE)
+
     if not net:
         raise exceptions.NetworkNotFound(net_id=net_id)
+    _raise_if_unauthorized(context.tenant_id, net)
 
     # NOTE (Perkins): If a device_id is given, try to prevent multiple ports
     # from being created for a device already attached to the network
