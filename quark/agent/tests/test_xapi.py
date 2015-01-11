a1c9cf8b765487d3767f5eeab399625f3ec8a5a7
Amir Sadoughi <amir.sadoughi@gmail.com>
2015-01-10 23:04:09 -0600

    Replace use of post_live_migrate with a new plugin
    RM11058

--- quark/agent/tests/test_xapi.py
+++ quark/agent/tests/test_xapi.py
@@ -1,5 +1,3 @@
-import json
-
 from quark.agent import xapi
 from quark.tests import test_base
 
@@ -43,17 +41,26 @@ class TestXapiClient(test_base.TestBase):
             "opaque1": {"other_config": {"nova_uuid": "uuid1"},
                         "power_state": "running", "is_a_template": False,
                         "is_control_domain": False,
-                        "name_label": "instance-1"},
+                        "name_label": "instance-1",
+                        "VIFs": ["opaque_vif1", "opaque_vif2"],
+                        "domid": "1"},
             "opaque2": {"other_config": {},
                         "power_state": "", "is_a_template": True,
                         "is_control_domain": True,
-                        "name_label": ""},
+                        "name_label": "",
+                        "VIFs": [],
+                        "domid": "2"},
         }
         instances = self.xclient.get_instances()
-        self.assertEqual(instances, {"opaque1": "uuid1"})
+        vm = xapi.VM(ref="opaque1", uuid="uuid1",
+                     dom_id="1", vifs=["opaque_vif1", "opaque_vif2"])
+        self.assertEqual(instances, {"opaque1": vm})
 
     def test_get_interfaces(self):
-        instances = {"opaque1": "device_id1"}
+        instances = {"opaque1": xapi.VM(uuid="device_id1",
+                                        ref="opaque1",
+                                        vifs=["opaque_vif1"],
+                                        dom_id="1")}
         self.session.xenapi.VIF.get_all_records.return_value = {
             "opaque_vif1": {"VM": "opaque1", "MAC": "00:11:22:33:44:55"},
             "opaque_vif2": {"VM": "opaque2", "MAC": "55:44:33:22:11:00"},
@@ -64,131 +71,102 @@ class TestXapiClient(test_base.TestBase):
                                        "opaque_vif1")]))
 
     def test_update_interfaces_added(self):
-        instances = {"opaque1": "device_id1"}
+        instances = {"opaque1": xapi.VM(uuid="device_id1",
+                                        ref="opaque1",
+                                        vifs=["opaque_vif1"],
+                                        dom_id="1")}
         interfaces = [xapi.VIF("device_id1", "00:11:22:33:44:55",
                                "opaque_vif1")]
-        location = "vm-data/networking/001122334455"
-        vm = {"xenstore_data": {location: json.dumps({"key": "value"})}}
-        self.session.xenapi.VM.get_record.return_value = vm
 
         self.xclient.update_interfaces(instances, interfaces, [], [])
 
         xenapi_VIF = self.session.xenapi.VIF
         xenapi_VIF.add_to_other_config.assert_called_once_with(
             "opaque_vif1", "security_groups", "enabled")
+        self.assertEqual(xenapi_VIF.remove_from_other_config.call_count, 0)
 
-        expected_args = dict(
-            host_uuid=self.session.xenapi.host.get_uuid.return_value,
-            uuid="device_id1")
+        expected_args = dict(dom_id="1", vif_index="0")
         self.session.xenapi.host.call_plugin.assert_called_once_with(
             self.session.xenapi.session.get_this_host.return_value,
-            "post_live_migrate", "instance_post_live_migration",
+            "neutron_vif_flow", "online_instance_flows",
             expected_args)
 
     def test_update_interfaces_added_vm_removed(self):
         instances = {}
         interfaces = [xapi.VIF("device_id1", "00:11:22:33:44:55",
                                "opaque_vif1")]
-        location = "vm-data/networking/001122334455"
-        vm = {"xenstore_data": {location: json.dumps({"key": "value"})}}
-        self.session.xenapi.VM.get_record.return_value = vm
 
         self.xclient.update_interfaces(instances, interfaces, [], [])
 
-        xenapi_VM = self.session.xenapi.VM
-        self.assertEqual(xenapi_VM.get_record.call_count, 0)
-        self.assertEqual(xenapi_VM.remove_from_xenstore_data.call_count, 0)
-        self.assertEqual(xenapi_VM.add_to_xenstore_data.call_count, 0)
+        xenapi_VIF = self.session.xenapi.VIF
+        xenapi_VIF.add_to_other_config.assert_called_once_with(
+            "opaque_vif1", "security_groups", "enabled")
+        self.assertEqual(xenapi_VIF.remove_from_other_config.call_count, 0)
 
-        expected_args_2 = dict(
-            host_uuid=self.session.xenapi.host.get_uuid.return_value,
-            uuid="device_id1")
-        self.session.xenapi.host.call_plugin.assert_has_calls([
-            mock.call(self.session.xenapi.session.get_this_host.return_value,
-                      "post_live_migrate", "instance_post_live_migration",
-                      expected_args_2)])
+        self.assertEqual(self.session.xenapi.host.call_plugin.call_count, 0)
 
     def test_update_interfaces_updated(self):
-        instances = {"opaque1": "device_id1"}
+        instances = {"opaque1": xapi.VM(uuid="device_id1",
+                                        ref="opaque1",
+                                        vifs=["opaque_vif1"],
+                                        dom_id="1")}
         interfaces = [xapi.VIF("device_id1", "00:11:22:33:44:55",
                                "opaque_vif1")]
-        location = "vm-data/networking/001122334455"
-        vm = {"xenstore_data": {location: json.dumps({"key": "value"})}}
-        self.session.xenapi.VM.get_record.return_value = vm
 
         self.xclient.update_interfaces(instances, [], interfaces, [])
 
-        xenapi_VM = self.session.xenapi.VM
-        self.assertEqual(xenapi_VM.get_record.call_count, 0)
-        self.assertEqual(xenapi_VM.remove_from_xenstore_data.call_count, 0)
-        self.assertEqual(xenapi_VM.add_to_xenstore_data.call_count, 0)
+        xenapi_VIF = self.session.xenapi.VIF
+        self.assertEqual(xenapi_VIF.add_to_other_config.call_count, 0)
+        self.assertEqual(xenapi_VIF.remove_from_other_config.call_count, 0)
 
-        expected_args_2 = dict(
-            host_uuid=self.session.xenapi.host.get_uuid.return_value,
-            uuid="device_id1")
-        self.session.xenapi.host.call_plugin.assert_has_calls([
-            mock.call(self.session.xenapi.session.get_this_host.return_value,
-                      "post_live_migrate", "instance_post_live_migration",
-                      expected_args_2)])
+        expected_args = dict(dom_id="1", vif_index="0")
+        self.session.xenapi.host.call_plugin.assert_called_once_with(
+            self.session.xenapi.session.get_this_host.return_value,
+            "neutron_vif_flow", "online_instance_flows",
+            expected_args)
 
     def test_update_interfaces_removed(self):
-        instances = {"opaque1": "device_id1"}
+        instances = {"opaque1": xapi.VM(uuid="device_id1",
+                                        ref="opaque1",
+                                        vifs=["opaque_vif1"],
+                                        dom_id="1")}
         interfaces = [xapi.VIF("device_id1", "00:11:22:33:44:55",
                                "opaque_vif1")]
-        location = "vm-data/networking/001122334455"
-        vm = {"xenstore_data": {location: json.dumps({
-            "security groups": "secure", "key": "value"})}}
-        self.session.xenapi.VM.get_record.return_value = vm
 
         self.xclient.update_interfaces(instances, [], [], interfaces)
 
         xenapi_VIF = self.session.xenapi.VIF
+        self.assertEqual(xenapi_VIF.add_to_other_config.call_count, 0)
         xenapi_VIF.remove_from_other_config.assert_called_once_with(
             "opaque_vif1", "security_groups")
 
-        expected_args = dict(
-            host_uuid=self.session.xenapi.host.get_uuid.return_value,
-            uuid="device_id1")
+        expected_args = dict(dom_id="1", vif_index="0")
         self.session.xenapi.host.call_plugin.assert_called_once_with(
             self.session.xenapi.session.get_this_host.return_value,
-            "post_live_migrate", "instance_post_live_migration",
+            "neutron_vif_flow", "online_instance_flows",
             expected_args)
 
     def test_update_interfaces_removed_vm_removed(self):
         instances = {}
         interfaces = [xapi.VIF("device_id1", "00:11:22:33:44:55",
                                "opaque_vif1")]
-        location = "vm-data/networking/001122334455"
-        vm = {"xenstore_data": {location: json.dumps({
-            "security groups": "secure", "key": "value"})}}
-        self.session.xenapi.VM.get_record.return_value = vm
 
         self.xclient.update_interfaces(instances, [], [], interfaces)
 
-        xenapi_VM = self.session.xenapi.VM
-        self.assertEqual(xenapi_VM.get_record.call_count, 0)
-        self.assertEqual(xenapi_VM.remove_from_xenstore_data.call_count, 0)
-        self.assertEqual(xenapi_VM.add_to_xenstore_data.call_count, 0)
+        xenapi_VIF = self.session.xenapi.VIF
+        self.assertEqual(xenapi_VIF.add_to_other_config.call_count, 0)
+        xenapi_VIF.remove_from_other_config.assert_called_once_with(
+            "opaque_vif1", "security_groups")
 
-        expected_args_2 = dict(
-            host_uuid=self.session.xenapi.host.get_uuid.return_value,
-            uuid="device_id1")
-        self.session.xenapi.host.call_plugin.assert_has_calls([
-            mock.call(self.session.xenapi.session.get_this_host.return_value,
-                      "post_live_migrate", "instance_post_live_migration",
-                      expected_args_2)])
+        self.assertEqual(self.session.xenapi.host.call_plugin.call_count, 0)
 
     def test_update_interfaces_none(self):
         instances = {"opaque1": "device_id1"}
-        location = "vm-data/networking/001122334455"
-        vm = {"xenstore_data": {location: json.dumps({"key": "value"})}}
-        self.session.xenapi.VM.get_record.return_value = vm
 
         self.xclient.update_interfaces(instances, [], [], [])
 
-        xenapi_VM = self.session.xenapi.VM
-        self.assertEqual(xenapi_VM.get_record.call_count, 0)
-        self.assertEqual(xenapi_VM.remove_from_xenstore_data.call_count, 0)
-        self.assertEqual(xenapi_VM.add_to_xenstore_data.call_count, 0)
+        xenapi_VIF = self.session.xenapi.VIF
+        self.assertEqual(xenapi_VIF.remove_from_other_config.call_count, 0)
+        self.assertEqual(xenapi_VIF.add_to_other_config.call_count, 0)
 
         self.assertEqual(self.session.xenapi.host.call_plugin.call_count, 0)
