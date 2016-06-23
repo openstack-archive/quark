5243de84043841a270fcf3ef893833c8f80a8250
Alexander Medvedev <xroot@yahoo.com>
2016-06-23 17:54:39 -0500

    QuarkIPAMLog test may fail due to timing
    JIRA: NCP-1975
    QuarkIPAMLog test may randomly fail due to timing
    
    Change-Id: Ia227d75d2983eb1d44c8b582821e200d2d11fe89
    Closes-Bug: 1595745

--- quark/tests/test_ipam.py
+++ quark/tests/test_ipam.py
@@ -2111,11 +2111,16 @@ class QuarkIpamTestLog(test_base.TestBase):
         log = quark.ipam.QuarkIPAMLog()
         entry1 = log.make_entry("test1")
         w = 10 / 1000.0
-        t = 0.005
         time.sleep(w)
         entry1.end()
         time_taken = entry1.get_time()
-        self.assertTrue(time_taken < w + t and time_taken > w - t)
+        # Can only assume that it took longer than the sleep time
+        self.assertTrue(time_taken >= w)
+        # Retrieved the entry - it's the first and only one
+        stored_entry = log.entries['test1'].pop()
+        # the start time should be before the end time
+        self.assertTrue(stored_entry.start_time < stored_entry.end_time,
+                        'Entry\'s start_time should be before end_time')
 
     def test_ipam_main_log_success(self):
         log = quark.ipam.QuarkIPAMLog()
