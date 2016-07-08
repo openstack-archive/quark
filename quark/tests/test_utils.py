598cd1f3235104d471b861119d4130eb23227e4e
Kyle Haley <kyle.haley@rackspace.com>
2016-07-08 15:23:38 -0700

    Add better exception handling in extensions
    Higher level exception handling lacked the capacity to catch
    unforeseen exceptions, which would sometimes result in program
    failure. Adding a decorator under utils to wrap functions in
    extensions to handle uncaught exceptions.
    
    Change-Id: Ie3c8398ce3c19dfd95b7f760ca607f448de0ccec
    JIRA:NCP-1984
    Closes-Bug: #1600387

--- quark/tests/test_utils.py
+++ quark/tests/test_utils.py
@@ -1,7 +1,9 @@
 import mock
 
+from neutron_lib import exceptions as n_exc
 from quark.tests import test_base
 from quark import utils
+import webob
 
 
 class TestRetryLoop(test_base.TestBase):
@@ -31,3 +33,45 @@ class TestRetryLoop(test_base.TestBase):
         ret = g()
         self.assertEqual(c.call_count, 2)
         self.assertEqual(ret, expected_ret)
+
+
+class TestExcWrapper(test_base.TestBase):
+
+    @utils.exc_wrapper
+    def raise_not_found(self):
+        raise n_exc.NotFound()
+
+    @utils.exc_wrapper
+    def raise_conflict(self):
+        raise n_exc.Conflict()
+
+    @utils.exc_wrapper
+    def raise_bad_request(self):
+        raise n_exc.BadRequest(resource="test_bad_request", msg="testing")
+
+    @utils.exc_wrapper
+    def raise_generic_exception(self):
+        raise Exception()
+
+    @utils.exc_wrapper
+    def no_raise(self):
+        return ""
+
+    def test_http_not_found(self):
+        with self.assertRaises(webob.exc.HTTPNotFound):
+            self.raise_not_found()
+
+    def test_http_conflict(self):
+        with self.assertRaises(webob.exc.HTTPConflict):
+            self.raise_conflict()
+
+    def test_http_bad_request(self):
+        with self.assertRaises(webob.exc.HTTPBadRequest):
+            self.raise_bad_request()
+
+    def test_http_server_error(self):
+        with self.assertRaises(webob.exc.HTTPInternalServerError):
+            self.raise_generic_exception()
+
+    def test_no_raise(self):
+        self.assertEqual("", self.no_raise())
