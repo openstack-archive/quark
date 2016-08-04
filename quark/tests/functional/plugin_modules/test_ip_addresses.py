59014be43023f50adb58fb0acbe06b5ee95cf735
Kyle Haley <kyle.haley@rackspace.com>
2016-08-03 17:45:52 -0700

    Add ability for admins to reserve ip addresses
    Give users with admin context the ability to reserve a deallocated
    ip address. Also, give users with admin context the ability to get
    information about IP addresses that have been deallocated.
    
    JIRA:NCP-1884
    Change-Id: I75358a1fd3b10af293a496d662020cc94b227ec4

--- quark/tests/functional/plugin_modules/test_ip_addresses.py
+++ quark/tests/functional/plugin_modules/test_ip_addresses.py
@@ -18,6 +18,8 @@ import contextlib
 import netaddr
 
 from quark.db import api as db_api
+from quark import exceptions as q_exc
+from quark.plugin_modules import ip_addresses as ip_addr
 from quark.tests.functional.base import BaseFunctionalTest
 
 
@@ -71,3 +73,126 @@ class QuarkGetIPAddresses(BaseFunctionalTest):
                 ip_address=[netaddr.IPAddress("192.168.10.2")],
                 scope=db_api.ALL)
             self.assertEqual(len(ip_addresses), 0)
+
+
+class QuarkTestReserveIPAdmin(BaseFunctionalTest):
+    def setUp(self):
+        super(QuarkTestReserveIPAdmin, self).setUp()
+        self.addr = netaddr.IPAddress("192.168.10.1")
+        self.ip_address_dealloc = {"ip_address": {"blah": "False"}}
+        self.ip_address_reserve = {"ip_address": {"deallocated": "False"}}
+
+    @contextlib.contextmanager
+    def _stubs(self):
+        with self.context.session.begin():
+            subnet = db_api.subnet_create(self.context,
+                                          cidr="192.168.0.0/24")
+            ip = db_api.ip_address_create(self.context,
+                                          address=self.addr,
+                                          subnet_id=subnet["id"])
+        yield ip
+
+    def test_reserve_ip_non_admin(self):
+        with self._stubs() as ip:
+            deallocated_ip = ip_addr.update_ip_address(self.context, ip["id"],
+                                                       self.ip_address_dealloc)
+            ip_address = db_api.ip_address_find(
+                self.context,
+                id=deallocated_ip["id"],
+                scope=db_api.ONE)
+            self.assertEqual(ip_address["deallocated"], True)
+            deallocated_ip = ip_addr.update_ip_address(self.context, ip["id"],
+                                                       self.ip_address_reserve)
+            ip_address = db_api.ip_address_find(
+                self.context,
+                id=deallocated_ip["id"],
+                scope=db_api.ONE)
+            self.assertEqual(ip_address["deallocated"], True)
+
+    def test_reserve_ip_admin(self):
+        self.context.is_admin = True
+        with self._stubs() as ip:
+            deallocated_ip = ip_addr.update_ip_address(self.context, ip["id"],
+                                                       self.ip_address_dealloc)
+            ip_address = db_api.ip_address_find(
+                self.context,
+                id=deallocated_ip["id"],
+                scope=db_api.ONE)
+            self.assertEqual(ip_address["deallocated"], True)
+            deallocated_ip = ip_addr.update_ip_address(self.context, ip["id"],
+                                                       self.ip_address_reserve)
+            ip_address = db_api.ip_address_find(
+                self.context,
+                id=deallocated_ip["id"],
+                scope=db_api.ONE)
+            self.assertEqual(ip_address["deallocated"], False)
+
+
+class QuarkTestGetDeallocatedIP(BaseFunctionalTest):
+    def setUp(self):
+        super(QuarkTestGetDeallocatedIP, self).setUp()
+        self.addr = netaddr.IPAddress("192.168.10.1")
+        self.ip_address_dealloc = {"ip_address": {"this": "deallocates"}}
+
+    @contextlib.contextmanager
+    def _stubs(self):
+        with self.context.session.begin():
+            subnet = db_api.subnet_create(self.context,
+                                          cidr="192.168.0.0/24")
+            ip = db_api.ip_address_create(self.context,
+                                          address=self.addr,
+                                          subnet_id=subnet["id"])
+        yield ip
+
+    def test_get_single_deallocated_ip_non_admin_raises(self):
+        with self._stubs() as ip:
+            reserved_ip = ip_addr.update_ip_address(self.context, ip["id"],
+                                                    self.ip_address_dealloc)
+            ip_address = db_api.ip_address_find(
+                self.context,
+                id=reserved_ip["id"],
+                scope=db_api.ONE)
+            self.assertEqual(ip_address["deallocated"], True)
+            with self.assertRaises(q_exc.IpAddressNotFound):
+                ip_addr.get_ip_address(self.context,
+                                       ip_address['id'])
+
+    def test_get_deallocated_ips_non_admin_empty(self):
+        with self._stubs() as ip:
+            reserved_ip = ip_addr.update_ip_address(self.context, ip["id"],
+                                                    self.ip_address_dealloc)
+            ip_address = db_api.ip_address_find(
+                self.context,
+                id=reserved_ip["id"],
+                scope=db_api.ONE)
+            self.assertEqual(ip_address["deallocated"], True)
+            deallocated_ips = ip_addr.get_ip_addresses(self.context)
+            self.assertEqual(len(deallocated_ips), 0)
+
+    def test_get_single_deallocated_ip_admin(self):
+        self.context.is_admin = True
+        with self._stubs() as ip:
+            reserved_ip = ip_addr.update_ip_address(self.context, ip["id"],
+                                                    self.ip_address_dealloc)
+            ip_address = db_api.ip_address_find(
+                self.context,
+                id=reserved_ip["id"],
+                scope=db_api.ONE)
+            self.assertEqual(ip_address["deallocated"], True)
+            deallocated_ip = ip_addr.get_ip_address(self.context,
+                                                    ip_address['id'])
+            self.assertEqual(reserved_ip['id'], deallocated_ip['id'])
+
+    def test_get_deallocated_ips_admin(self):
+        self.context.is_admin = True
+        with self._stubs() as ip:
+            reserved_ip = ip_addr.update_ip_address(self.context, ip["id"],
+                                                    self.ip_address_dealloc)
+            ip_address = db_api.ip_address_find(
+                self.context,
+                id=reserved_ip["id"],
+                scope=db_api.ONE)
+            self.assertEqual(ip_address["deallocated"], True)
+            deallocated_ips = ip_addr.get_ip_addresses(self.context)
+            self.assertEqual(len(deallocated_ips), 1)
+            self.assertEqual(reserved_ip['id'], deallocated_ips[0]['id'])
